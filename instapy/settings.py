7323f5712b205cdd654cbe311ebe72467499a5a2
slavpetroff <slavpetroff@gmail.com>
2018-08-23 17:18:11 +0300

    Monkeypatch the chromedriver location

--- instapy/settings.py
+++ instapy/settings.py
@@ -2,6 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 def get_os_env():
     from sys import platform
     if platform == "linux" or platform == "linux2":
@@ -11,28 +12,34 @@ def get_os_env():
     elif platform == "win32":
         return 'windows'
 
+
 class Settings:
     log_location = os.path.join(BASE_DIR, 'logs')
     database_location = os.path.join(BASE_DIR, 'db', 'instapy.db')
     os_env = get_os_env()
 
-    chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
+    # TODO: Make it dynamic
+    chromedriver_location = '/usr/local/share/chromedriver'
 
     if os_env == 'osx':
-        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_osx')
+        chromedriver_location = os.path.join(
+            BASE_DIR, 'assets', 'chromedriver_osx')
 
     if os_env == 'linux':
-        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_linux')
+        # TODO: Make it dynamic
+        chromedriver_location = '/usr/local/share/chromedriver'
 
     if os_env == 'windows':
-        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver_windows')
+        chromedriver_location = os.path.join(
+            BASE_DIR, 'assets', 'chromedriver_windows')
 
     if not os.path.exists(chromedriver_location):
-        chromedriver_location = os.path.join(BASE_DIR, 'assets', 'chromedriver')
+        chromedriver_location = os.path.join(
+            BASE_DIR, 'assets', 'chromedriver')
 
     chromedriver_min_version = 2.36
     # Set a logger cache outside the InstaPy object to avoid re-instantiation issues
     loggers = {}
     logger = None
     # Set current profile credentials for DB operations
-    profile = {"id":None, "name":None}
+    profile = {"id": None, "name": None}
